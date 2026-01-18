99348fccb2958a3b2b7f80121c0588f7bd365529
zanadoman <domanseven@gmail.com>
2026-01-18 22:09:53 +0100

    postgres connection

--- api/main.js
+++ api/main.js
@@ -3,9 +3,15 @@
 import express from 'express';
 import path from 'path';
 import '@dotenvx/dotenvx/config';
+import { Client } from 'pg';
 
 const app = express();
 const root = path.resolve('dist/sop/browser');
 app.use(express.static(root));
 app.use((_, res) => res.sendFile(`${root}/index.html`));
 app.listen(process.env.APP_PORT || 8080);
+
+const client = new Client(process.env.DATABASE_URL);
+client.connect().then(() => {
+  console.log('connected');
+});
